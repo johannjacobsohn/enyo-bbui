f6a98a87f095852ed6c7d9464d7a3f5ae13a6ea4
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-07-17 19:44:03 +0200

    Add documentation and sample to bbUI.Checkbox

---
+++ samples/Checkbox.js
@@ -0,0 +1,18 @@
+enyo.kind({
+	name: "bbUI.sample.Checkbox",
+	classes: "enyo-fit",
+	handleChange: function(inSender, inEvent){
+		this.$.second.setChecked(inSender.getValue());
+	},
+	components: [
+		{kind: "bbUI.Init", controlsDark: true, listsDark: false},
+		{content: "bbUI.Checkbox:"},
+		{kind: "bbUI.Checkbox", onActivate: "handleChange"},
+		{content: "onyx.Checkbox:"},
+		{kind: "onyx.Checkbox", onActivate: "handleChange"},
+		{content: "enyo.Checkbox:"},
+		{kind: "enyo.Checkbox", onActivate: "handleChange"},
+
+		{kind: "bbUI.Checkbox", name: "second"}
+	]
+});
--- source/bbUI.Checkbox.js
+++ source/bbUI.Checkbox.js
@@ -1,12 +1,19 @@
+/**
+	The _bbUI.Checkbox_ implements a [bbUI.js
+	Checkbox](https://github.com/blackberry/bbUI.js/wiki/Checkboxes) as a
+	[enyo.Checkbox](http://enyojs.com/api/#enyo.Checkbox) which is
+	interchangable with [onyx.Checkbox](http://enyojs.com/api/#onyx.Checkbox).
+ */
 enyo.kind({
 	name: "bbUI.Checkbox",
 	kind: "enyo.Checkbox",
+	//* @protected
 	attributes: {
 		onchange: "enyo.$[this.id].handleChange()"
 	},
 	handleChange: function(){
-		this.checked = this.node.getChecked();
-		this.doActivate();
+		this.checked = this.active = this.node.getChecked();
+		this.doActivate({originator: this});
 	},
 	checkedChanged: function(inOldValue){
 		if(typeof inOldValue === "undefined"){
